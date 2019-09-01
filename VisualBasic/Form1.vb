6832569803863395933a2efbafd4b1b4d7a15384
itrascastro <itrascastro@gmail.com>
2019-09-01 12:34:28 +0200

    Updated

--- VisualBasic/Form1.vb
+++ VisualBasic/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        TextBox1.Text = "Hello World 1"
+        TextBox1.Text = "Hello World"
     End Sub
 End Class
