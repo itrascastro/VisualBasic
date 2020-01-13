0060377d174342206dcff0c28d64940ff4249752
itrascastro <itrascastro@gmail.com>
2020-01-13 20:26:25 +0100

    Suma de N primeros aleatorios

--- Proyecto2/Arrays1D.Designer.vb
+++ Proyecto2/Arrays1D.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Arrays1D
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -43,11 +44,21 @@ Partial Class Arrays1D
         Me.Button1.Text = "Ejercicio1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 24)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Ejercicio2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Arrays1D
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "Arrays1D"
@@ -59,4 +70,5 @@ Partial Class Arrays1D
 
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Proyecto2/Arrays1D.vb
+++ Proyecto2/Arrays1D.vb
@@ -11,4 +11,21 @@
             TextBox1.Text += n + ", " + n2 + ", " + n3 + vbNewLine
         Next
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim num(14) As Integer
+        Dim Random As New Random()
+        Dim i As Integer
+        Dim N As Integer = 4
+        Dim sum As Integer = 0
+
+        For i = 0 To 14
+            num(i) = Random.Next(-50, 50)
+        Next
+
+        For i = 0 To N
+            sum += num(i)
+        Next
+
+    End Sub
 End Class
